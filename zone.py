7a594a41a40dafc5cf434b79e760726d39695742
danwms3 <danwms@yahoo.com>
2016-11-10 13:55:55 -0600

    fixed zone.py

--- zone.py
+++ zone.py
@@ -8,22 +8,21 @@ import json
 Modify these please
 """
 url='http://172.22.163.35:8080/ins'
-switchuser='admin'
-switchpassword='nbv_12345'
+switchuser='danwms'
+switchpassword='AICteam'
 
 debug=0
 
-myheaders={'content-type':'application/json-rpc'}
-payload=[
-  {
-    "jsonrpc": "2.0",
-    "method": "cli",
-    "params": {
-      "cmd": "show zoneset active"an 10
-      "version": 1.2
-    },
-    "id": 1
+myheaders={'content-type':'application/json'}
+payload={
+  "ins_api": {
+    "version": "1.2",
+    "type": "cli_show",
+    "chunk": "0",
+    "sid": "1",
+    "input": "show zoneset active vsan 10",
+    "output_format": "json"
   }
-]
+}
 response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
 print response
